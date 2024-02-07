b63b35eefe23293b7273a1ba279ed28e2bbc67fa
RevanHUB <davidasir2020@gmail.com>
2024-02-07 19:11:02 +0000

    chore: working bootstrap and sass

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    Launch demo modal
+  </button>
+  <button id="login"> Login button</button>
+  <button id="map"> Login button</button>
+</template>
+
+<style scoped>
+
+</style>
