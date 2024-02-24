62e306c22d1db6550363fc69a1931a97ee74d0db
AntonioJPL <ajpl1903@gmail.com>
2024-02-24 12:25:27 +0000

    primera version terminada

---
+++ src/assets/posts.js
@@ -0,0 +1,37 @@
+export const posts = [{
+    "title": "Oferta de ordenador gaming",
+    "image": "/ordenador.webp",
+    "desc": "Tenemos nuevas ofertas en ordenadores gaming desde 399.99€. Si estás buscando un equipo nuevo no dudes en pasarte a ver nuestras ofertas o solicitar tu presupuesto a medida. ¡Te esperamos!",
+    "type": "Oferta", 
+    "tags" : ["#Mejoresprecios","#Ordenador", "#Gaming"],
+    "time": "45 min"
+}, {
+    "title": "Viernes sin igic",
+    "image": null,
+    "desc": "Solo este viernes 23 de febrero todos nuestros productos de telefonía estarán sin igic, si necesitas renovar tu teléfono aprovecha esta oportunidad.",
+    "type": "Evento",
+    "tags" : ["#Telefono","#Smartphone", "#Sinigic"],
+    "time": "12 h"
+}, {
+    "title": "Perifericos Logitech 25%",
+    "image": "/perifericos.webp",
+    "desc": "Hasta el viernes 1 de Marzo tendremos un 25% de desucuento en todos nuestros productos de la marca Logitech, ¡Ahora es el momento de volverse gamer!",
+    "type": "Oferta", 
+    "tags" : ["#Logitech","#Descuento", "#Perifericos"],
+    "time": "18 h" 
+},{
+    "title": "Presupuestos a medida",
+    "image": "/setup.webp",
+    "desc": "Si te quieres montar un set-up gamer con el máximo rendimiento y al mejor precio no dudes en pasarte por nuestro local a solicitar presupuesto, ¡Te aseguramos cumplir con tus expectativas!",
+    "type": "Info", 
+    "tags" : ["#Presupuesto","#Setup"],
+    "time": "1 d"
+}, {
+    "title": "Nuevo monitor dell",
+    "image": "/monitor.webp",
+    "desc": "¡Nos acaba de llegar! Si no quieres perderte ni un solo detalle de tus partidas, ven a probar el nuevo monitor Dell con 240Hz de tasa de refresco",
+    "type": "Info", 
+    "tags" : ["#Monitor","#Dell", "#240hz"],
+    "time": "2 d"
+}
+];
---
+++ src/components/ProfilePicture.vue
@@ -0,0 +1,8 @@
+<script setup>
+const props = defineProps({
+    source: String
+})
+</script>
+<template>
+<img class="profilePic" :src="source" alt="Imágen de perfil">
+</template>
